122656a813fea389beb77f0d62995c4ac69ce835
Entisar <Code.g.light@gmail.com>
2025-05-08 12:19:49 +0300

    Combined the switch into the if block.

--- lab3.js
+++ lab3.js
@@ -1,5 +1,5 @@
-let button=document.getElementsByClassName("button");
-let buttonsArray=Array.from(button);
+let button = document.getElementsByClassName("button");
+let buttonsArray = Array.from(button);
 console.log(buttonsArray);
 
 let trafficLight = document.getElementById("traffic-light");
@@ -8,32 +8,18 @@ let trafficLightBulbs = trafficLight.children;
 let trafficLightBulbsArray = Array.from(trafficLightBulbs);
 console.log(trafficLightBulbsArray);
 
-
 buttonsArray.forEach((button) => {
-    button.addEventListener("click", () => {
-        if (button.id === "stopButton") {
-            color = "red";
-        }
-        else if (button.id === "slowButton") {
-            color = "yellow";
-        } else if (button.id === "goButton") {
-            color = "green";
-        }
-
-        trafficLightBulbsArray.forEach((bulb) => {
-            bulb.style.backgroundColor = ""; 
-        });
-
-        switch (color) {
-            case "red":
-                trafficLightBulbsArray[0].style.backgroundColor = "red";
-                break;
-            case "yellow":
-                trafficLightBulbsArray[1].style.backgroundColor = "yellow";
-                break;
-            case "green":
-                trafficLightBulbsArray[2].style.backgroundColor = "green";
-                break;
-        }
+  button.addEventListener("click", () => {
+    trafficLightBulbsArray.forEach((bulb) => {
+      bulb.style.backgroundColor = "";
     });
+
+    if (button.id === "stopButton") {
+      trafficLightBulbsArray[0].style.backgroundColor = "red";
+    } else if (button.id === "slowButton") {
+      trafficLightBulbsArray[1].style.backgroundColor = "yellow";
+    } else if (button.id === "goButton") {
+      trafficLightBulbsArray[2].style.backgroundColor = "green";
+    }
+  });
 });
